feeecd9b7ca5573f27dd96951b06c6f438ec4636
ashitakem-cell <ashitakem@gmail.com>
2026-07-06 06:23:00 +0000

    yes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbopack: {
+      root: "../", // Ye tumhare project ke root ko point karega
+    },
+  },
 };
 
 export default nextConfig;
